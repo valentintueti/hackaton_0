2495923894a4a120a815403899df232d2449e558
Valentín Tuesta <valentin.tuesta@utec.edu.pe>
2025-04-15 16:59:47 -0500

    Update calculate fucntion

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
-def calculate() -> float:
+def calculate(operation:str) -> float:
+    info = operation.split()
+    a = info[0]
+    b = info[2]
+
+    op=info[1]
+
+    if op=="+":
+        suma(a,b)
+    elif op=="-"
+        resta(a,b)
+    elif op=="*"
+        multiplicacion(a,b)
+    else:
+        division(a,b)
     pass
 
 
