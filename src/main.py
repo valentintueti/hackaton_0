9286766e8205a503d45fdca78b35c0004a1d8947
Marco Soto Maceda <marco.soto.m@utec.edu.pe>
2025-04-15 17:14:33 -0500

    fixed calculate function

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
-def calculate(operation:str) -> float:
+def calculate(operation: str) -> float:
     info = operation.split()
-    a = info[0]
-    b = info[2]
+    if len(info) != 3:
+        raise ValueError("Operación no válida. Usa el formato: número operador número (ej. 2 + 2)")
 
-    op=info[1]
+    a = float(info[0])
+    b = float(info[2])
+    op = info[1]
 
-    if op=="+":
-        suma(a,b)
-    elif op=="-"
-        resta(a,b)
-    elif op=="*"
-        multiplicacion(a,b)
+    if op == "+":
+        return suma(a, b)
+    elif op == "-":
+        return resta(a, b)
+    elif op == "*":
+        return multiplicacion(a, b)
+    elif op == "/":
+        return division(a, b)
     else:
-        division(a,b)
-    pass
+        raise ValueError("Operador no reconocido")
 
 
 def suma(a, b):
